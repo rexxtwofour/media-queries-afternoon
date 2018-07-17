35a4298fbf28171be27b2ca853dcddff5a1fdb3f
Eric Rivera <ericrivera@Eric-Riveras-MacBook-Pro.local>
2018-07-16 17:12:51 -0700

    assignment turnin

---
+++ media-queries-afternoon/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <div className="startbootstrap">Start Bootstrap</div>
+        <nav>
+          <div className="bigNav">SERVICES</div>
+          <div className="bigNav">PORTFOLIO</div>
+          <div className="bigNav">ABOUT</div>
+          <div className="bigNav">TEAM</div>
+          <div className="bigNav">CONTACT</div>
+        </nav>
+
+    
+         </header>
+      <div className="banner">
+
+              <div className="intro">
+              <div className="welcome">Welcome To Our Studio!</div>
+              <div className="meetYou">IT'S NICE TO MEET YOU</div>
+              <button className="tellMe">TELL ME MORE</button>
+              </div>
+
+            <button className="navbar-toggler navbar-toggler-right"></button>
+
+
+      </div>
+
+      <body>
+        
+      </body>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
